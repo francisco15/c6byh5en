ae03180347a0ee4b413103dc9d3a23a2229a01ee
francisco15 <franciscohuertas38@gmail.com>
2017-03-10 14:21:42 -0500

    Agregado crear productos con JSON

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,11 +1,25 @@
 module Api
 	module V1
 		class ProductsController < ApplicationController
-
+			protect_from_forgery with: :null_session
+			
 			def index
 				render json: Product.all
 			end
 
+			def create
+				product = Product.new(product_params)
+				if product.save
+					render json: product, status: :created
+				else
+					render json: {errors: product.errors}, status: :unprocessable_entity
+				end
+			end
+
+			private
+				def product_params
+					params.require(:product).permit(:name, :price)
+				end
 		end
 	end
 end
